b01c553ec49c7fefd7fb41b9d43e51b5330255a2
CarolinaCapilla <carolina.capilla@gmail.com>
2021-03-04 14:09:15 +0000

    built links to projects websites + source code. Cards working on responsive

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -14,75 +14,119 @@ const Portfolio = () => {
         </h2>
       </div>
       <div className="grid gap-6 row-gap-5 mb-8 lg:grid-cols-3 sm:row-gap-6 sm:grid-cols-2">
-        {/* <a
-          href="https://your-burger-builder.herokuapp.com/"
-          target="_blank"
-          rel="noreferrer"
-          aria-label="View Item"
-        > */}
         <div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
+          <p className="text-lg font-bold text-center text-purple-900">
+            Burger Builder
+          </p>
           <img
             className="object-cover w-full h-56 md:h-64 xl:h-80"
             src={burger}
             alt=""
           />
           <div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
-            <p className="mb-4 text-lg font-bold text-gray-100">Mona Lisa</p>
-            <p className="text-sm tracking-wide text-gray-300">
-              Painted between 1503 and 1517, Da Vinci’s alluring portrait has
-              been dogged by two questions since the day it was made: Who’s the
-              subject and why is she smiling?
+            <p className="tracking-wide text-sm text-gray-300 mb-7 md:mb-20">
+              Build a yummy burger with different ingredients to then get it
+              delivered right to your door. <br />
+              Built with React.js, JSES6, CSS3 and Firebase. Hosted in Heroku
             </p>
+            <div className="grid">
+              <a
+                href="https://your-burger-builder.herokuapp.com/"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="View Item"
+                className="md:px-4 md:py-2 px-2 py-1 mb-3 text-center font-medium tracking-wide text-white transition-colors duration-200 transform bg-purple-600 rounded-md dark:bg-gray-800 hover:bg-purple-900 dark:hover:bg-gray-700"
+              >
+                See live
+              </a>
+              <a
+                href="https://github.com/CarolinaCapilla/burger-builder"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="View Item"
+                className="md:px-4 md:py-2 px-2 py-1 mb-3 text-center font-medium tracking-wide text-white transition-colors duration-200 transform bg-purple-400 rounded-md dark:bg-gray-800 hover:bg-purple-900 dark:hover:bg-gray-700"
+              >
+                Source code
+              </a>
+            </div>
           </div>
         </div>
-        {/* </a> */}
-        <a
-          href="https://art-deco-finder.herokuapp.com/"
-          target="_blank"
-          rel="noreferrer"
-          aria-label="View Item"
-        >
-          <div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
-            <img
-              className="object-cover w-full h-56 md:h-64 xl:h-80"
-              src={decor}
-              alt=""
-            />
-            <div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
-              <p className="mb-4 text-lg font-bold text-gray-100">
-                The Starry Night
-              </p>
-              <p className="text-sm tracking-wide text-gray-300">
-                Vincent Van Gogh’s most popular painting, The Starry Night was
-                created by Van Gogh at the asylum in Saint-Rémy, where he’d
-                committed himself in 1889.
-              </p>
+        <div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
+          <p className="text-lg font-bold text-center text-purple-900">Decor</p>
+          <img
+            className="object-cover w-full h-56 md:h-64 xl:h-80"
+            src={decor}
+            alt=""
+          />
+          <div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+            <p className="tracking-wide text-sm text-gray-300 mb-2 md:mb-20">
+              Stylish artistic rentals solution for art owners, connecting them
+              with customers that wish to rent a piece of art to enrich their
+              environment. <br />
+              Built with Ruby on Rails, HTML, CSS, JavaScript, Active Record and
+              hosted in Heroku.
+            </p>
+            <div className="grid">
+              <a
+                href="https://art-deco-finder.herokuapp.com/"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="View Item"
+                className="md:px-4 md:py-2 px-2 py-1 mb-3 text-center font-medium tracking-wide text-white transition-colors duration-200 transform bg-purple-600 rounded-md dark:bg-gray-800 hover:bg-purple-900 dark:hover:bg-gray-700"
+              >
+                See live
+              </a>
+              <a
+                href="https://github.com/CarolinaCapilla/al-decor"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="View Item"
+                className="md:px-4 md:py-2 px-2 py-1 mb-3 text-center font-medium tracking-wide text-white transition-colors duration-200 transform bg-purple-400 rounded-md dark:bg-gray-800 hover:bg-purple-900 dark:hover:bg-gray-700"
+              >
+                Source code
+              </a>
             </div>
           </div>
-        </a>
-        <a
-          href="https://themenu-mudita.herokuapp.com/"
-          target="_blank"
-          rel="noreferrer"
-          aria-label="View Item"
-        >
-          <div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
-            <img
-              className="object-cover w-full h-56 md:h-64 xl:h-80"
-              src={themenu}
-              alt=""
-            />
-            <div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
-              <p className="mb-4 text-lg font-bold text-gray-100">The Kiss</p>
-              <p className="text-sm tracking-wide text-gray-300">
-                Opulently gilded and extravagantly patterned, The Kiss, Gustav
-                Klimt’s fin-de-siècle portrayal of intimacy, is a mix of
-                Symbolism and Vienna Jugendstil, the Austrian variant of Art
-                Nouveau.
-              </p>
+        </div>
+        <div className="relative overflow-hidden transition duration-200 transform rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl">
+          <p className="text-lg font-bold text-center text-purple-900">
+            The Menu
+          </p>
+          <img
+            className="object-cover w-full h-56 md:h-64 xl:h-80"
+            src={themenu}
+            alt=""
+          />
+          <div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+            <p className="tracking-wide text-sm text-gray-300 mb-4 md:mb-20">
+              Empowering chefs and bartenders to be their own bosses providing
+              them with a platform to showcase their skills.
+              <br />
+              Built with Ruby on Rails, HTML, CSS, AOS. JavaScript, Active
+              Record and hosted on Heroku.
+            </p>
+            <div className="grid">
+              <a
+                href="https://themenu-mudita.herokuapp.com/"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="View Item"
+                className="md:px-4 md:py-2 px-2 py-1 mb-3 text-center font-medium tracking-wide text-white transition-colors duration-200 transform bg-purple-600 rounded-md dark:bg-gray-800 hover:bg-purple-900 dark:hover:bg-gray-700"
+              >
+                See live
+              </a>
+              <a
+                href="https://github.com/CarolinaCapilla/the_menu"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="View Item"
+                className="md:px-4 md:py-2 px-2 py-1 mb-3 text-center font-medium tracking-wide text-white transition-colors duration-200 transform bg-purple-400 rounded-md dark:bg-gray-800 hover:bg-purple-900 dark:hover:bg-gray-700"
+              >
+                Source code
+              </a>
             </div>
           </div>
-        </a>
+        </div>
       </div>
     </div>
   );
